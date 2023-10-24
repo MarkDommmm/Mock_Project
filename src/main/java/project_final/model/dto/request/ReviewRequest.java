983c6799b4eacc0aad6aa9cd11547822a366aa5c
Longduong1994 <dglong1994@gmail.com>
2023-10-24 08:39:15 +0700

    update

---
+++ src/main/java/project_final/model/dto/request/ReviewRequest.java
@@ -0,0 +1,23 @@
+package project_final.model.dto.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.web.multipart.MultipartFile;
+import project_final.entity.User;
+
+
+import java.util.Date;
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class ReviewRequest {
+    private Long id;
+    private MultipartFile image;
+    private String comment;
+    private int rating;
+    private Date createdDate;
+    private User user;
+}
